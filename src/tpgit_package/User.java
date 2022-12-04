a6a1533da56c1ccb042a3441afa6da6b81919560
MessaoudLefouili <MessaoudLefouili@gmail.com>
2022-12-04 22:32:33 +0100

    Created user class

---
+++ src/tpgit_package/User.java
@@ -0,0 +1,14 @@
+package tpgit_package;
+
+public class User {
+
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
